28e0cd72cef9dc5c71bc54d776fb38cfb7b40c1a
Patrick-FRE <patrick.lin@antra.com>
2022-09-09 10:57:09 -0400

    update-grid

--- src/ProductGrid.js
+++ src/ProductGrid.js
@@ -7,7 +7,7 @@ export const ProductGrid = ({ heading, cta1, p1Name, p1Img, p1Price, p2Name, p2I
     <div className="elc-product-grid-items">
       <div className="elc-product-grid-items__item">
         <ProductImage image={p1Img} />
-        <div>
+        <div className="elc-product-grid-items__item__des">
           <h1>{p1Name}</h1>
           <h4>{p1Price}</h4>
           <button className="elc-btn elc-btn-outlined">{cta1}</button>
@@ -15,7 +15,7 @@ export const ProductGrid = ({ heading, cta1, p1Name, p1Img, p1Price, p2Name, p2I
       </div>
       <div className="elc-product-grid-items__item">
         <ProductImage image={p2Img} />
-        <div >
+        <div className="elc-product-grid-items__item__des">
           <h1>{p2Name}</h1>
           <h4>{p2Price}</h4>
           <button className="elc-btn elc-btn-outlined">{cta1}</button>
@@ -23,7 +23,7 @@ export const ProductGrid = ({ heading, cta1, p1Name, p1Img, p1Price, p2Name, p2I
       </div>
       <div className="elc-product-grid-items__item">
         <ProductImage image={p3Img} />
-        <div>
+        <div className="elc-product-grid-items__item__des">
           <h1>{p3Name}</h1>
           <h4>{p3Price}</h4>
           <button className="elc-btn elc-btn-outlined">{cta1}</button>
@@ -31,7 +31,7 @@ export const ProductGrid = ({ heading, cta1, p1Name, p1Img, p1Price, p2Name, p2I
       </div>
       <div className="elc-product-grid-items__item">
         <ProductImage image={p4Img} />
-        <div>
+        <div className="elc-product-grid-items__item__des">
           <h1>{p4Name}</h1>
           <h4>{p4Price}</h4>
           <button className="elc-btn elc-btn-outlined">{cta1}</button>
